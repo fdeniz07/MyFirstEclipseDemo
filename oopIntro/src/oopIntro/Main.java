f9973475856e5320eb0dfe36f5f2d212e8a60511
fdeniz07 <deniz.fatih@hotmail.de>
2021-04-28 23:39:04 +0200

    Day 3

--- oopIntro/src/oopIntro/Main.java
+++ oopIntro/src/oopIntro/Main.java
@@ -1,48 +1,30 @@
 package oopIntro;
 
-import java.util.Iterator;
-
 public class Main {
 
 	public static void main(String[] args) {
 
-		Product product1=new Product(1,"Lenovo V14",15000,"16 GB Ram"); //referans olusturma,instance
+		Product product1 = new Product(1, "Lenovo V14", 15000, "16 GB Ram", 10); // referans olusturma,instance
 
-		
-		Product product2=new Product();
-		product2.id=2;
-		product2.name="Lenovo V15";
-		product2.unitPrice=16000;
-		product2.detail="32 GB Ram";
-		
-		Product product3=new Product();
-		product3.id=3;
-		product3.name="HP 5";
-		product3.unitPrice=10000;
-		product3.detail="8 GB Ram";
-		
-		Product[] products= {product1,product2,product3};
-		
-		for (Product product:products) {
-			System.out.println(product.name);
-		}
-		
-		
-		System.out.println(products.length);
-		
-		
+		Product product2 = new Product();
+		product2.setId(2);
+		product2.setName("Lenove V15");
+		product2.setDetail("16 GB Ram");
+		product2.setDiscount(10);
+		product2.setUnitPrice(16000);
+
+		System.out.println(product2.getUnitPriceAfterDiscount());
+
 		Category category1 = new Category();
-		category1.id=1;
-		category1.name="Computer";
-		
+		category1.setId(1);
+		category1.setName("Icecek");
+
 		Category category2 = new Category();
-		category2.id=2;
-		category2.name="Home & Garden";
-		
-		ProductManager productManager = new ProductManager();
-		productManager.addToCart(product1);
-		productManager.addToCart(product2);
-		productManager.addToCart(product3);
+		category2.setId(2);
+		category2.setName("Yiyecek");
+
+		System.out.println(category1.getName());
+		System.out.println(category2.getName());
 	}
 
 }
