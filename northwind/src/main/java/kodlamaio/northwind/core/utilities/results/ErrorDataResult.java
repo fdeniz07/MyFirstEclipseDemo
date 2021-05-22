9ec67f697076ebaa4bfd51825b5ce240c4765546
Fatih Deniz <deniz.fatih@hotmail.de>
2021-05-22 23:46:48 +0200

    lesson 7.1

---
+++ northwind/src/main/java/kodlamaio/northwind/core/utilities/results/ErrorDataResult.java
@@ -0,0 +1,22 @@
+package kodlamaio.northwind.core.utilities.results;
+
+public class ErrorDataResult <T> extends DataResult<T>{
+
+	public ErrorDataResult(T data, String message) {
+		super(data,false, message); //base class'a 3 parametreden birini; false bilgisini manuel yolluyoruz
+	}
+	
+	
+	public ErrorDataResult(T data) {
+		super(data, false); //base class'a 2 parametreden birini; false bilgisini manuel yolluyoruz
+	}
+	
+	public ErrorDataResult(String message) {
+		super(null, false, message); //base class'a 3 parametreden ikisini manuel yolluyoruz
+	}
+	
+	public ErrorDataResult() {
+		super(null, false); //base class'a 2 parametreden ikisini manuel yolluyoruz
+	}
+	
+}
