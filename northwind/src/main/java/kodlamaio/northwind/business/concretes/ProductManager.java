bec6c924eb11802da4cdc1dfa1d66e7c08280a53
Fatih Deniz <deniz.fatih@hotmail.de>
2021-05-23 23:34:55 +0200

    lesson 7.1

--- northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
 import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
+import kodlamaio.northwind.core.utilities.results.SuccessResult;
 import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
 import kodlamaio.northwind.entities.concretes.Product;
 
@@ -27,6 +29,12 @@ public class ProductManager implements ProductService {
 
 		return new SuccessDataResult<List<Product>>(this.productDao.findAll(), "Data Listelendi");
 
+	}
+
+	@Override
+	public Result add(Product product) {
+		this.productDao.save(product);
+		return new SuccessResult("Ürün eklendi");
 	}
 
 }
